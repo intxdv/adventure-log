04fa27ab9385aae82d464b1aac56b0ee60e932f6
Taki <syafiqabiyyutaqi123@gmail.com>
2026-09-15 01:46:16 +0700

    fix(mobile): refine section 01 scrolling, carousel controls, compact archive modal, solid hero typography, and footer drop shadow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -162,13 +162,13 @@ export const Footer: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <defs>
-              <filter id="slvgnt-drop-shadow" x="0" y="0" width="2427.76" height="477.35" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+              <filter id="slvgnt-drop-shadow" x="-30%" y="-30%" width="160%" height="160%" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
                 <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                <feOffset dy="4"/>
-                <feGaussianBlur stdDeviation="4"/>
+                <feOffset dx="0" dy="6"/>
+                <feGaussianBlur stdDeviation="8"/>
                 <feComposite in2="hardAlpha" operator="out"/>
-                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.55 0"/>
                 <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
                 <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
               </filter>
